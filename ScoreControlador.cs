78e07dd89411cee4bb68a610ee2482ce4498295d
Juan Carlos Campos <rapidclimate@gmail.com>
2018-10-01 10:06:57 -0500

    Add files via upload

---
+++ ScoreControlador.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreControlador : MonoBehaviour {
+   public  AudioSource fuenteMoneda;
+    public AudioClip clipCaja;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "monedita")
+        {
+            fuenteMoneda.PlayOneShot(clipCaja);
+            //SE destruye
+            Destroy(other.gameObject);
+        }
+    }
+}
