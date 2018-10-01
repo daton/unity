e5816616732b0d7f35a57326b15f26aa34805daf
Juan Carlos Campos <rapidclimate@gmail.com>
2018-10-01 10:05:54 -0500

    Add files via upload

---
+++ MonedaControlador.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonedaControlador : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // print("se imprime a lo loco");
+
+        transform.Rotate(Vector3.up * 60 * Time.deltaTime, Space.World);
+    }
+}
