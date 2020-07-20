93640a5f6bd06c0a927136fd2b388d1100cf9026
Juan Carlos Campos <rapidclimate@gmail.com>
2020-07-20 09:32:45 -0500

    Add files via upload

--- CICLO-20-03/AnimacionesZombis.cs
+++ CICLO-20-03/AnimacionesZombis.cs
@@ -18,6 +18,22 @@ public class AnimacionesZombis : MonoBehaviour
 
     //Aqui va el numero inicial de vidas vamos a usar porentaje el maximo sera 100%
    public int maximoVidas = 100;
+    public bool estaMuerto = false;
+
+
+    //Aqui vamos a declarar un objeto de juego que NO ESTARA EN EL ESCENARIO, ya que sera un prefab.
+    public GameObject prefabYaku;
+
+    //Aqui vamos a generar un objeto para manipular ese prefab, lo interesante es que
+    //este puede ser un ARREGLO DE ZOMBIES, ES DECIR UNA HORDA DE ZOMBI!!
+    // NO es necesario que sea publico este objeto porque solo estara en el codigo
+    GameObject zombiClonado;
+
+    //Fuente disparo
+   public AudioSource fuenteDisparo;
+
+    //Clip Disparo
+    public AudioClip clipDisparo;
     
    
 
@@ -27,10 +43,21 @@ public class AnimacionesZombis : MonoBehaviour
         // en el script BarraSaludable
         barra.setMaximoVidas(maximoVidas);
 
+        //Vamos a  probar si se hace bien la clonacion en la localizacion del cubo.
+    zombiClonado=    Instantiate(prefabYaku, new Vector3(308, 0.85f, 380.85f), Quaternion.identity);
+
     }
 
     void Update()
     {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            print("Disparaste");
+           
+        
+            fuenteDisparo.PlayOneShot(clipDisparo);
+        }
 
         //Mostramos la distancia entre el zombi y el player
       float distancia=  Vector3.Distance(yaku.transform.position, transform.position);
@@ -39,26 +66,49 @@ public class AnimacionesZombis : MonoBehaviour
 
 
         //Vamos a hacer que el zombi yaku nos observe de forma inteligente
+        //Hacemos que el zombi clonado voltee a vernos igual que el yaku original
 
-        yaku.transform.LookAt(transform.position);
+  
+        zombiClonado.transform.LookAt(transform.position);
+
 
         //La siguiente linea hace que el zombi no siga hacia donde nos movamos
         //Antes de que se mueva ponemos la condicion, aqui vamos a usar 5 metros para mas facil
-      
-        if (distancia < 5)
+        //El zombi clonado siempre nos va a seguir, independientemente de cualquier codigo
+       // zombiClonado.transform.position = Vector3.MoveTowards(zombiClonado.transform.position, transform.position, Time.deltaTime * 2.5f);
+
+
+        if (distancia < 5  && !estaMuerto )
         {
             //Aqui le decimos que se anime con el estado de maqui de correr
+
             yaku.GetComponent<Animator>().Play("Running");
-           
-            yaku.transform.position = Vector3.MoveTowards(yaku.transform.position, transform.position, Time.deltaTime * 2.5f);
+            yaku.transform.LookAt(transform.position);
+
+
+            //Checamos que la distancia sea poca y si disparamos
+            if (distancia < 1.5f && Input.GetMouseButtonDown(0))
+            {
+         
+                estaMuerto = true;
+                yaku.GetComponent<Animator>().Play("morir");
+            }
+            else
+            {
+                yaku.transform.position = Vector3.MoveTowards(yaku.transform.position, transform.position, Time.deltaTime * 2.5f);
+            }
+
+        }
+    
+        else if(!estaMuerto)
+        {
+            yaku.GetComponent<Animator>().Play("Idle");
         }
         else
         {
-            yaku.GetComponent<Animator>().Play("Idle");
-            
+            yaku.GetComponent<Animator>().Play("morir");
         }
-        
-   
+         
         
     }
     //Vamos a implementar en esta sección un metodo que funciona
